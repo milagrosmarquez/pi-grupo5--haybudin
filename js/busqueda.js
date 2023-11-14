8a4dd0f74789d729bf81f5d65affd93f9ccbf661
akpaz <akpaz@udesa.edu.ar>
2023-11-14 15:14:59 -0300

    agrego arhcivo de electivas

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,2 +1,13 @@
-const busqueda = document.querySelector("#busqueda");
+let acaVaLaAPIKey = "e55171de8d3da1084e5f5aa7654d69f7";
+
+let queryS = location.search;
+let qsObj = new URLSearchParams(queryS)
+let busq = qsObj.get("Busqueda");
+
+let urlPeliculasB = `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${busqueda}`;
+let urlSeriesB = `https://api.themoviedb.org/3/search/tv?api_key=${acaVaLaAPIKey}&query=${busqueda}`;
+
+console.log(urlPeliculasB);
+console.log(urlSeriesB);
+
 
