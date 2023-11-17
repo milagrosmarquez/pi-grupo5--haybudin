fa719e9bda5380dea7b5d7208186d453d3654b9e
Lilayasuff <lyasuff@udesa.edu.ar>
2023-11-17 15:38:50 -0300

    peliculas,agregue el logo en todos,empece busqueda

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,3 +1,23 @@
+let apiKey = "e55171de8d3da1084e5f5aa7654d69f7";
 
+let qs = location.search;
+let qsobj = new URLSearchParams (qs)
+let busqueda=qsobj.get("busqueda")
+console.log(busqueda);
+let url=`https://api.themoviedb.org/3/search/movie?query=${busqueda}&include_adult=false&language=en-US&page=1?api_key=${apiKey}`
+
+let resu = document.querySelector ("#resulta")
+i = 0
+
+fetch(url)
+.then(function(res){
+    return res.json();
+})
+.then(function(data){
+    console.log(data);
+    
+})
+
+.catch()
 
 
